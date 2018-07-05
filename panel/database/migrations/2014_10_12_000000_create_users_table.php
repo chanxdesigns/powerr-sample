8a6e9dd58339eaf27c5e929eb756e8309260d2ce
Wangbren Meitei <chanx.designs@gmail.com>
2018-07-05 12:03:32 +0530

    Removed unique index from email column

--- panel/database/migrations/2014_10_12_000000_create_users_table.php
+++ panel/database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,7 +16,7 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->string('id');
             $table->string('name');
-            $table->string('email')->unique();
+            $table->string('email');
             $table->string('password');
             $table->integer('age');
             $table->string('gender');
